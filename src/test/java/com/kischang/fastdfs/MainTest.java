56a180cc1b17886847220d126c1c7079a2b0b3a8
KisChang <734615869@qq.com>
2016-12-23 19:01:29 +0800

    修复测试代码中的bug

--- src/test/java/com/kischang/fastdfs/MainTest.java
+++ src/test/java/com/kischang/fastdfs/MainTest.java
@@ -1,14 +1,12 @@
 package com.kischang.fastdfs;
 
-import org.csource.fastdfs.StorageClient;
+import com.kischang.fastdfs.exception.FastDFSException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * 连接池测试
@@ -22,27 +20,35 @@ public class MainTest {
 
     @Resource
     private FastDFSTemplate dfsTemplate;
+    @Resource
+    private FastDFSTemplateFactory factory;
 
     @Test
-    public void testMain() throws InterruptedException {
-        final Set<Integer> set = new HashSet<>();
+    public void testUploadAndDel() throws FastDFSException {
+        System.out.println(factory.getG_tracker_group());
 
+        FastDfsRv rv = dfsTemplate.upload("".getBytes(), "txt");
+        System.out.println(rv);
+        dfsTemplate.deleteFile(rv);
+    }
+
+    @Test
+    public void testPool() throws InterruptedException {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                StorageClient client = dfsTemplate.getClient();
-                System.out.println((set.contains(client.hashCode()) ? "OLD": "NEW") + " >>> " + Thread.currentThread().getName() + ">>>" + client.hashCode());
                 try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException ignored) { }
-                set.add(client.hashCode());
-                dfsTemplate.releaseClient(client);
+                    FastDfsRv rv = dfsTemplate.upload("123123213".getBytes(), "txt");
+                    System.out.println(rv);
+                    dfsTemplate.deleteFile(rv);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         };
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 100; i++) {
             new Thread(runnable).start();
-            Thread.sleep(500);
         }
         Thread.currentThread().join();
     }
